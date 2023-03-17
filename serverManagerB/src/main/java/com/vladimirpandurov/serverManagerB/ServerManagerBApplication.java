4f7ef8de3dd22f4068ba588782c28c4c32980270
panvla <pandvlado@gmail.com>
2023-03-17 05:00:53 +0100

    commit all.

---
+++ serverManagerB/src/main/java/com/vladimirpandurov/serverManagerB/ServerManagerBApplication.java
@@ -0,0 +1,13 @@
+package com.vladimirpandurov.serverManagerB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerManagerBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerManagerBApplication.class, args);
+	}
+
+}
--- serverManagerF/src/app/app.module.ts
+++ serverManagerF/src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NotificationModule } from './notification.module';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    NotificationModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
