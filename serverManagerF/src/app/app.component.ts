71b066dec8ce389611f18e3487bd60fb37fc4c52
panvla <pandvlado@gmail.com>
2023-03-17 08:34:13 +0100

    done.

--- serverManagerF/src/app/app.component.ts
+++ serverManagerF/src/app/app.component.ts
@@ -1,7 +1,15 @@
 import { Statement } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
-import { catchError, map, Observable, of, startWith } from 'rxjs';
+import {
+  BehaviorSubject,
+  catchError,
+  map,
+  Observable,
+  of,
+  startWith,
+} from 'rxjs';
 import { DataState } from './enum/data_state.enum';
+import { Status } from './enum/status.enum';
 import { AppState } from './interface/app-state';
 import { CustomResponse } from './interface/custom-response';
 import { ServerService } from './service/server.service';
@@ -13,12 +21,18 @@ import { ServerService } from './service/server.service';
 })
 export class AppComponent implements OnInit {
   appState$: Observable<AppState<CustomResponse>>;
+  readonly DataState = DataState;
+  readonly Status = Status;
+  private filterSubject = new BehaviorSubject<string>('');
+  private dataSubject = new BehaviorSubject<CustomResponse>(null);
+  filterStatus$ = this.filterSubject.asObservable();
 
   constructor(private serverService: ServerService) {}
 
   ngOnInit(): void {
     this.appState$ = this.serverService.servers$().pipe(
       map((response: CustomResponse) => {
+        this.dataSubject.next(response);
         return { dataState: DataState.LOADED_STATE, appData: response };
       }),
       startWith({ dataState: DataState.LOADING_STATE }),
@@ -26,5 +40,51 @@ export class AppComponent implements OnInit {
         return of({ dataState: DataState.ERROR_STATE, error: error });
       })
     );
+  }
+
+  pingServer(ipAddress: string): void {
+    this.filterSubject.next(ipAddress);
+    this.appState$ = this.serverService.ping$(ipAddress).pipe(
+      map((response: CustomResponse) => {
+        this.dataSubject.value.data.servers[
+          this.dataSubject.value.data.servers.findIndex(
+            (server) => server.id === response.data.server.id
+          )
+        ] = response.data.server;
+        this.filterSubject.next('');
+        return {
+          dataState: DataState.LOADED_STATE,
+          appData: this.dataSubject.value,
+        };
+      }),
+      startWith({
+        dataState: DataState.LOADED_STATE,
+        appData: this.dataSubject.value,
+      }),
+      catchError((error: string) => {
+        this.filterSubject.next('');
+        return of({ dataState: DataState.ERROR_STATE, error: error });
+      })
+    );
+  }
+
+  filterServers(status: Status): void {
+    this.appState$ = this.serverService
+      .filter$(status, this.dataSubject.value)
+      .pipe(
+        map((response) => {
+          return {
+            dataState: DataState.LOADED_STATE,
+            appData: response,
+          };
+        }),
+        startWith({
+          dataState: DataState.LOADED_STATE,
+          appData: this.dataSubject.value,
+        }),
+        catchError((error: string) => {
+          return of({ dataState: DataState.ERROR_STATE, error });
+        })
+      );
   }
 }
